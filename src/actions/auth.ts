e9976d87a4538f45020512b489f68601d1dacac8
Alireza <alirezarashnoo85@gmail.com>
2026-07-31 16:31:38 +0330

    feat: implement user authentication with registration and login functions

---
+++ src/actions/auth.ts
@@ -0,0 +1,46 @@
+"use server";
+
+import { prisma } from "@/lib/prisma";
+import bcrypt from "bcrypt";
+import { signIn } from "@/auth";
+
+export async function registerUser(data: {
+  name: string;
+  email: string;
+  password: string;
+}) {
+  const existingUser = await prisma.user.findUnique({
+    where: {
+      email: data.email,
+    },
+  });
+
+  if (existingUser) {
+    throw new Error("User already exists");
+  }
+
+  const passwordHash = await bcrypt.hash(data.password, 12);
+
+  await prisma.user.create({
+    data: {
+      name: data.name,
+      email: data.email,
+      passwordHash,
+    },
+  });
+
+  // Auto Login
+  await signIn("credentials", {
+    email: data.email,
+    password: data.password,
+    redirectTo: "/",
+  });
+}
+
+export async function loginUser(data: { email: string; password: string }) {
+  await signIn("credentials", {
+    email: data.email,
+    password: data.password,
+    redirectTo: "/",
+  });
+}
