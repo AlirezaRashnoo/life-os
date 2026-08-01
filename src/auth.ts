142356c140db61cd4c4f888503b42f72ebb56a7c
Alireza <alirezarashnoo85@gmail.com>
2026-08-01 19:36:55 +0330

    feat: implement authentication handlers and current user retrieval function

--- src/auth.ts
+++ src/auth.ts
@@ -1,3 +1,71 @@
+// import NextAuth from "next-auth";
+
+// import Credentials from "next-auth/providers/credentials";
+// import Google from "next-auth/providers/google";
+
+// import { PrismaAdapter } from "@auth/prisma-adapter";
+
+// import { prisma } from "@/lib/prisma";
+
+// import bcrypt from "bcrypt";
+
+// export const { handlers, auth, signIn, signOut } = NextAuth({
+//   adapter: PrismaAdapter(prisma),
+
+//   session: {
+//     strategy: "jwt",
+//   },
+
+//   providers: [
+//     Credentials({
+//       name: "credentials",
+
+//       credentials: {
+//         email: {},
+//         password: {},
+//       },
+
+//       async authorize(credentials) {
+//         if (!credentials?.email || !credentials?.password) {
+//           return null;
+//         }
+
+//         const user = await prisma.user.findUnique({
+//           where: {
+//             email: credentials.email as string,
+//           },
+//         });
+
+//         if (!user?.passwordHash) {
+//           return null;
+//         }
+
+//         const passwordMatch = await bcrypt.compare(
+//           credentials.password as string,
+//           user.passwordHash,
+//         );
+
+//         if (!passwordMatch) {
+//           return null;
+//         }
+
+//         return {
+//           id: user.id,
+//           email: user.email,
+//           name: user.name,
+//           image: user.image,
+//         };
+//       },
+//     }),
+
+//     Google({
+//       clientId: process.env.GOOGLE_CLIENT_ID!,
+
+//       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+//     }),
+//   ],
+// });
+
 import NextAuth from "next-auth";
 
 import Credentials from "next-auth/providers/credentials";
@@ -14,6 +82,24 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
 
   session: {
     strategy: "jwt",
+  },
+
+  callbacks: {
+    async jwt({ token, user }) {
+      if (user) {
+        token.id = user.id;
+      }
+
+      return token;
+    },
+
+    async session({ session, token }) {
+      if (session.user) {
+        session.user.id = token.id as string;
+      }
+
+      return session;
+    },
   },
 
   providers: [
@@ -60,7 +146,6 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
 
     Google({
       clientId: process.env.GOOGLE_CLIENT_ID!,
-
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
     }),
   ],
